db845e13abb2e9813ce55963bd08bdda18b7e2c5
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-06-19 19:33:06 +0200

    Removed Display and Player components, updated App and Sidebar components to use react-router-dom, and made corresponding changes to CSS and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-import Player from './components/Player'; 
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <div>
-      <div className="container">
+    <BrowserRouter>
+      <div className="app-container">
         <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Add other routes here */}
+          </Routes>
+        </div>
       </div>
-      <Player />
-    </div>
-  );  
+    </BrowserRouter>
+  );
 };
 
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../assets/assets';
-import '../index.css' 
+import { useNavigate } from 'react-router-dom';
+import '../index.css';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  const goToHome = () => {
+    navigate('/');
+  };
+
+  const goToPodcasts = () => {
+    navigate('/podcasts'); 
+  };
+
+  const toggleSearchBar = () => {
+    setShowSearchBar(!showSearchBar);
+  };
+
   return (
     <div className='sidebar sidebar-lg'>
       <div className='top-section'>
-        <div className='top-section-item'>
-          <img className='top-section-icon' src={assets.home_icon} alt='' />
+        <div className='top-section-item' onClick={goToHome}>
+          <img className='top-section-icon' src={assets.home_icon} alt='Home' />
           <p className='top-section-text'>Home</p>
         </div>
-        <div className='top-section-item'>
-          <img className='top-section-icon' src={assets.search_icon} alt='' />
+        <div className='top-section-item' onClick={toggleSearchBar}>
+          <img className='top-section-icon' src={assets.search_icon} alt='Search' />
           <p className='top-section-text'>Search</p>
         </div>
+        {showSearchBar && (
+          <div className='search-bar'>
+            <input type='text' placeholder='Search...' className='search-input' />
+          </div>
+        )}
       </div>
       <div className='bottom-section'>
         <div className='bottom-section-content'>
-          <div className='bottom-section-item'>
-            <img className='bottom-section-icon-large' src={assets.stack_icon} alt='' />
-            <p className='bottom-section-text'>Your Library</p>
+          <div className='bottom-section-item' onClick={goToPodcasts}>
+            <img className='bottom-section-icon-large' src={assets.stack_icon} alt='Podcasts' />
+            <p className='bottom-section-text'>Your Podcasts</p>
           </div>
           <div className='bottom-section-item'>
-            <img className='bottom-section-icon-small' src={assets.arrow_icon} alt='' />
-            <img className='bottom-section-icon-small' src={assets.plus_icon} alt='' />
+            <img className='bottom-section-icon-small' src={assets.arrow_icon} alt='Arrow' />
+            <img className='bottom-section-icon-small' src={assets.plus_icon} alt='Plus' />
           </div>
         </div>
         <div className='playlist'>
@@ -39,6 +60,6 @@ const Sidebar = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
